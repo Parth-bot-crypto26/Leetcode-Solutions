a96f66f9f876ff266c391ce7ab40b3615e8dc4db
DP <145845508+Parth-bot-crypto26@users.noreply.github.com>
2026-07-31 08:32:32 +0530

    Add files via upload

---
+++ 83_Greatest_Common_Divisor_of_Strings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2 != str2+str1) return "";
+        int n1 = str1.length();
+        int n2 = str2.length();
+        int gcd_length = gcd(n1, n2);
+        string ans = str1.substr(0, gcd_length);
+        return ans;
+    }
+};

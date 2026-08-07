832e65d14831567e27e14830695fa56d6afb2972
DP <145845508+Parth-bot-crypto26@users.noreply.github.com>
2026-08-07 20:37:58 +0530

    Add files via upload

---
+++ 94_Pascal's_Triangle_II.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector row(rowIndex+1, 1);
+        long long current = 1;
+        for (int i = 1; i < rowIndex; ++i) {
+            current = current * (rowIndex - i + 1) / i;
+            row[i] = current;
+        }
+        return row;
+    }
+};
